07baa34b7852b784322150b3c5b6c964e74b27b8
ape9 <santerikettu@outlook.com>
2023-02-27 23:59:53 +0200

    slightly improved mining algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-
+use std::time::{SystemTime, UNIX_EPOCH};
 use sha256::{digest};
 
 fn create_hash(input: String) -> String {
@@ -8,35 +8,53 @@ fn create_hash(input: String) -> String {
 
 struct Proof {
     nonce: i32,
-    result_hash: String
+    result_hash: String,
+    timestamp: u64
 }
 
 fn find_proof(prev_hash: String, difficulty: usize) -> Proof {
-    let mut found_proof: bool = false;
     let mut nonce = 0;
     let mut result_hash = String::new();
+    let mut timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
     
     loop {
-        let input_str = format!("{}{}", prev_hash, nonce);
+        let input_str = format!("{}{}{}", prev_hash, nonce, timestamp);
         let hash = create_hash(input_str);
-        println!("Hash: {}", hash);
+        
         if hash.as_str().starts_with(&"0".repeat(difficulty)) {
+            // OK, solution was found.
             result_hash = hash;
             break;
         }
 
+        if nonce == i32::MAX {
+            // reset nonce and get a new timestamp when max size of i32 is reached
+            // to continue searching
+            nonce = 0;
+            timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+        }
         nonce += 1;
     }
 
     Proof {
         nonce,
-        result_hash
+        result_hash,
+        timestamp
     }
 }
 
+
 fn main() {
     let hash = digest(b"suomi");
-    let proof = find_proof(hash, 4);
+    let proof = find_proof(hash, 5);
     println!("Successfully mined {}", proof.result_hash);
     println!("Nonce: {}", proof.nonce);
+    println!("Timestamp: {}", proof.timestamp);
+
 }
